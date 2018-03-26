fbb1eb0124abfd5fe5f62804422fe41149904ef2
myhome98304 <myhome98304@gmail.com>
2018-03-26 23:34:27 +0900

    update do_PS-MCL

--- do_PS-MCL.sh
+++ do_PS-MCL.sh
@@ -1,7 +1,7 @@
 cd $(dirname $(readlink -f $0))
 
 
-./PS-MCL.sh dataset/bionetowork/BioGrid-homo/homo ./ -sc 3 1.5 -reg 4 1 0 0.7
+./PS-MCL.sh dataset/bionetwork/BioGrid-homo/homo ./ -sc 3 1.5 -reg 4 1 0 0.7
 ./measure.sh homo_PS-MCL-CoarseLevel-3_numThread-4.assign homo_nodemap dataset/reference/homo/homo -row 1000 2
 
  

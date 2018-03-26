781ef69e312fa1018efa4490b2a999a7642d3eb3
myhome98304 <myhome98304@gmail.com>
2018-03-26 20:36:06 +0900

    demo

--- do_PS-MCL.sh
+++ do_PS-MCL.sh
@@ -1,7 +1,7 @@
 cd $(dirname $(readlink -f $0))
 
 
-./PS-MCL.sh dataset/BioGrid/Saccharomyces/sacc.txt ./ -sc 2 1.5 -reg 4 1 0 0.7
+./PS-MCL.sh dataset/BioGrid/Saccharomyces/sacc.txt ./ -sc 3 1.5 -reg 4 1 0 0.7
 
 
  
